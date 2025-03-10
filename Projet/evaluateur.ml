a936d0ac1f1e545c47afb8dd268a54d3cc263269
luminary <lmboyenval@free.fr>
2025-03-10 12:55:17 +0100

    modification du typage des fonctions

--- Projet/evaluateur.ml
+++ Projet/evaluateur.ml
@@ -1,10 +1,10 @@
 (* Fonction prenant en paramtre une expression et
 fourni un résultat en sortie *)
-(* a -> b *)
+(* 'expr -> b *)
 let eval_expr expr = failwith "Not yet implemented"
 
 (* Prend en parametre un programme et affiche
 la valeur produite par l'évaluation de la fonction
-main (retourne soit un entier soit un booléen) *)
-(* a -> bool | int *)
+main vrai si l'évaluation est correcte, faux sinon*)
+(* 'programme -> bool *)
 let eval_prog prog  = failwith "Not yet implemented"
--- Projet/verif.ml
+++ Projet/verif.ml
@@ -1,13 +1,27 @@
-
+open Syntax
 (*Que font les fonctions*)
 (* vérifie qu'une expression à un type donné *)
-(*  'a -> 'b -> bool*)
-let verif_expr expr t = failwith "Not yet implemented"
+(*  'expr -> 'b -> bool*)
+let verif_expr expr t env_type= failwith "Not yet implemented"
+
 
 (* vérifie que la déclaration des fonctions est correcte *)
-(*  'a -> bool*)
+(*  'fun_decl -> bool*)
 let verif_decl_fun fonction = failwith "Not yet implemented"
 
 (* retourne vrai si le programme est ien typé sinon faux *)
-(*  'a -> bool*)
+(*  'programme -> 'typ *)
 let verif_prog simpleML = failwith "Not yet implemented"
+
+
+(* type utilisé pour vérifie le typage  *)
+(* au début nous avons une liste vide
+dès qu'une variable est trouvé, on l'ajoute ainsi 
+que son type  *)
+(* Associe un type a une variable *)
+type env_type = (idvar * typ) list
+
+(* type utilisé pour vérifié l'évalution des expressions*)
+(* Associe une valeur a une variable *)
+type env_exp = (idfun * typ) list
+
