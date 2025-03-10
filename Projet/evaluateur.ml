2f817e833df0b496086b1ac8ef3693a29125c73d
1+e^iπ = 0 <eliot.belouin@etu.univ-nantes.fr>
2025-03-10 15:07:27 +0100

    correction du prof

--- Projet/evaluateur.ml
+++ Projet/evaluateur.ml
@@ -2,11 +2,11 @@
 
 (* Fonction prenant en parametre une expression et
 fourni un résultat en sortie *)
-(* 'expr -> b *)
-let eval_expr expr = failwith "Not yet implemented"
+(* 'expr -> valeur *)
+let eval_expr expr env_fonction env_val= failwith "Not yet implemented"
 
 (* Prend en parametre un programme et affiche
 la valeur produite par l'évaluation de la fonction
 main vrai si l'évaluation est correcte, faux sinon*)
-(* 'programme -> bool *)
+(* 'programme -> unit() *)
 let eval_prog prog  = failwith "Not yet implemented"
