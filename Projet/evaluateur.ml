e7062df09f23a8cf000d9ae7467e36dc7219d480
deatheed <95500649+deatheed@users.noreply.github.com>
2025-03-22 13:31:37 +0100

    Update evaluateur.ml

--- Projet/evaluateur.ml
+++ Projet/evaluateur.ml
@@ -2,23 +2,31 @@
 open Syntax
 
 (* Fonction prenant en paramètre une expression et fournissant un résultat en sortie *)
-(* expr -> (idvar * valeur) list -> (idfun * fun_decl) list -> valeur  *)
 let rec eval_expr (e : expr) (env : (idvar * valeur) list) (env_fun : (idfun * fun_decl) list) =
   match e with
   | Var x -> (try List.assoc x env with Not_found -> failwith ("Variable non définie : " ^ x))
   | Int n -> VInt n
   | Bool b -> VBool b
+  | Float f -> VFloat f
+
+  (* Section pour tous les binaryOP*)
   | BinaryOp (op, e1, e2) -> begin
       let v1 = eval_expr e1 env env_fun in
       let v2 = eval_expr e2 env env_fun in
       match op, v1, v2 with
+
+      (* BinaryOP pour les int*)
       | Plus, VInt i1, VInt i2 -> VInt (i1 + i2)
       | Minus, VInt i1, VInt i2 -> VInt (i1 - i2)
       | Mult, VInt i1, VInt i2 -> VInt (i1 * i2)
       | Div, VInt i1, VInt i2 when i2 <> 0 -> VInt (i1 / i2)
       | Div, _, VInt 0 -> failwith "Division par zéro"
+
+      (* BinaryOP pour les operateurs booleens ou & et*)
       | And, VBool b1, VBool b2 -> VBool (b1 && b2)
       | Or, VBool b1, VBool b2 -> VBool (b1 || b2)
+
+      (* BinaryOP pour les tests d'égalités *)
       | Equal, VInt i1, VInt i2 -> VBool (i1 = i2)
       | Equal, VBool b1, VBool b2 -> VBool (b1 = b2)
       | NEqual, VInt i1, VInt i2 -> VBool (i1 <> i2)
@@ -27,24 +35,40 @@ let rec eval_expr (e : expr) (env : (idvar * valeur) list) (env_fun : (idfun * f
       | LessEq, VInt i1, VInt i2 -> VBool (i1 <= i2)
       | Great, VInt i1, VInt i2 -> VBool (i1 > i2)
       | GreatEq, VInt i1, VInt i2 -> VBool (i1 >= i2)
-      | PlusF, VFloat n1, VFloat n2 -> VFloat (n1 +. n2) 
+
+      (* BinaryOP pour les floats*)
+      | PlusF, VFloat f1, VFloat f2 -> VFloat (f1 +. f2) 
+      | MinusF, VFloat f1, VFloat f2 -> VFloat (f1 -. f2) 
+      | MultF, VFloat f1, VFloat f2 -> VFloat (f1 *. f2) 
+      | DivF, VFloat f1, VFloat f2 when f2 <> 0. -> VFloat (f1 /. f2) 
+      | DivF, _, VFloat 0. -> failwith "Division par zéro"
+
+      (* BinaryOP pour les int*)
       | _, _, _ -> failwith "Opération sur des types incompatibles"
     end
+
+
   | UnaryOp (Not, e1) -> begin
       match eval_expr e1 env env_fun with
       | VBool b -> VBool (not b)
       | _ -> failwith "Opération booléenne sur un type incompatible"
     end
+
+
   | If (e1, e2, e3) -> begin
       match eval_expr e1 env env_fun with
       | VBool true -> eval_expr e2 env env_fun
       | VBool false -> eval_expr e3 env env_fun
       | _ -> failwith "Condition d'un if-then-else non booléenne"
     end
+
+
   | Let (x, _, e1, e2) ->
       let v1 = eval_expr e1 env env_fun in
       let new_env = (x, v1) :: env in
       eval_expr e2 new_env env_fun
+
+
   | App (fname, args) -> 
       (match List.assoc_opt fname env_fun with
       | Some func_decl ->
@@ -53,11 +77,19 @@ let rec eval_expr (e : expr) (env : (idvar * valeur) list) (env_fun : (idfun * f
           let new_env = List.combine param_names arg_values @ env in
           eval_expr func_decl.corps new_env env_fun
       | None -> failwith ("Fonction non définie : " ^ fname))
-  | Seq (_, _) -> failwith "to do"
-  | _ -> failwith "to do"
 
-(*    Affiche une valeur sur la sortie standard *)
-(* valeur -> unit *)
+
+  | Seq (_, _) -> failwith "to do1"
+
+
+  | _ -> failwith "to do2"
+
+
+
+
+
+
+
 let print_valeur valeur =
   match valeur with
   | VInt x -> print_int x; print_newline()
@@ -65,8 +97,6 @@ let print_valeur valeur =
   | VFloat x -> print_float x; print_newline()
   | _ -> failwith "Valeur non prise en charge"
 
-(*  Évalue un programme en exécutant la fonction 'main' si elle est définie. *)
-(* programme -> unit *)
 let eval_prog (p : programme) =
   let env_fun = List.map (fun f -> (f.id, f)) p in
   match List.assoc_opt "main" env_fun with
